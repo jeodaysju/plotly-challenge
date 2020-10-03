06ab04ed610a186de49c5ba97afc6880a06b99ce
jeodaysju <jeodaysju@yahoo.com>
2020-10-02 19:56:41 -0500

    Almost done

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,21 +1,104 @@
 // Some of the code was derived from office hours and working together on process
 
 // function to draw the bubble chart
-function DrawBubbleChart(sampleID)
-{
-    // console.log(`DrawBubbleChart(${sampleID})`);
-}
+// function DrawBubbleChart(sampleID)
+// {
+//     // console.log(`DrawBubbleChart(${sampleID})`);
+//     // Load the data and then ...
+//     d3.json("samples.json").then((data) => {
+
+//         // Get the names
+//         var sampleNames = data.names;
+
+//         var samples = data.samples;
+//         var resultArray = samples.filter(s => s.id == sampleID);
+//         var result = resultArray[0];
+
+//         var otu_ids = result.otu_ids;
+//         var otu_labels = result.otu_labels;
+//         var sample_values = result.sample_values;
+
+//         var bubbleData = {
+//             x: otu_ids,
+//             y: sample_values,
+//             type: "scatter",
+//             mode: "markers",
+//             marker: {
+//                 size: sample_values,
+//                 sizemode: 'area',
+//                 color:
+//             },
+//             text: otu_labels
+//         }
+
+//         var bubbleLayout = {
+//             title: "Top 10 Bacteria Cultures Found",
+//             margin:{t: 30, l: 150}
+//         }
+        
+//         Plotly.newPlot("scatter", [bubbleData], bubbleLayout)
+//         });
+// }
 
 // function to draw the bar chart 
 function DrawBarChart(sampleID)
 {
     // console.log(`DrawBarChar(${sampleID})`);
+    // Load the data and then ...
+    d3.json("samples.json").then((data) => {
+
+        // Get the names
+        var sampleNames = data.names;
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleID);
+        var result = resultArray[0];
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        var yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
+
+        var barData = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0, 10).reverse(),
+            orientation: "h"
+        }
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin:{t: 30, l: 150}
+        }
+        
+        Plotly.newPlot("bar", [barData], barLayout)
+        });
+
 }
 
 // Show Metadata
 function ShowMetadata(sampleID)
 {
     console.log(`ShowMetadata(${sampleID})`);
+
+    d3.json("samples.json").then((data) => {
+
+        var metadata = data.metadata;
+        var resultArray = metadata.filter(md => md.id == sampleID);
+        var result = resultArray[0];
+
+        var panel = d3.select("#sample-metadata");
+        panel.html("");
+
+        Object.entries(result).forEach(([key, value]) => {
+            // var textToShow = 'Shirley was here!';
+
+            panel.append("h6").text(`${key}: ${value}`);
+
+        });
+
+    });
 }
 
 // function for when drop down changes - this acts as event handler for optionChanged
@@ -53,7 +136,7 @@ function InitDashboard()
 
         // Draw Graphs
         DrawBarChart(sampleID);
-        DrawBubbleChart(sampleID);
+        // DrawBubbleChart(sampleID);
 
         // Display metadata
         ShowMetadata(sampleID);
